fd3aecb1c96a5d80b031c2079ea0574d9406321a
Gary James <gjames@edgetechacademy.edu>
2021-01-12 07:58:56 -0600

    Reduced the number of files to the bare minimum. And leave in a few errors for the class to find

--- src/main/java/com/example/falljavafinal/ListingsApplication.java
+++ src/main/java/com/example/falljavafinal/ListingsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FalljavafinalApplication {
+public class ListingsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FalljavafinalApplication.class, args);
+        SpringApplication.run(ListingsApplication.class, args);
     }
 
 }
--- src/main/java/com/example/falljavafinal/controllers/ListingsController.java
+++ src/main/java/com/example/falljavafinal/controllers/ListingsController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/listings")
@@ -29,70 +31,76 @@ public class ListingsController {
     //      If you want to see all the attributes of our Listing object they are listed below
     //          propertyType,address,city,zip,price,beds,baths,location,sqFt,lotSize,
     //          yearBuilt,daysOnMarket,pricePerSqFt,hoa,link,latitude,longitude
-    /*
-            There are 70 points to be earned in adding code to this controller
-                There are 10 TODO's todo
-                There is a possibility of getting an extra 5 points
-    */
     @RequestMapping("/")
     public String list(Model model) {
-        //      5 points
-        //          TODO #1 add an attribute to be used to set the Title of the web page
+        model.addAttribute("title", "Dallas Property Listings");
         model.addAttribute("criteria", "All Properties");
         model.addAttribute("listings", listings);
-        return "listListings";
+        return "listings";
     }
 
     //  entry point provided for you for the search request in the listing page
     //      initially in only receives the city name. You will add more to it in this exam
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    //      5 points
-    //          TODO #2 Add in the extra search fields that are mentioned below
-    //          in TODO's #3 - #10
-    public String search(@RequestParam String city, Model model) {
+    public String search(@RequestParam String city, @RequestParam Integer price,
+                         @RequestParam Integer sqFt, @RequestParam Integer beds, Model model) {
         List<Listing> matchedHomes = listings;
 
-        String criteria = "NOT YET IMPLEMENTED -- WRITE SOME CODE";
+        String criteria = "Found Matching Listings:";
 
         if (city != null && city.length() > 0) {
-        //      5 points
-        //          TODO #3     call a method to find all listings that match our search city
-            criteria += " for " + city;             //  this will build a string to show what we have searched for
+            matchedHomes = matchedHomes.stream().filter(l -> l.getCity().indexOf(city) >= 0).collect(Collectors.toList());
+            criteria += " for City: " + city;
         }
 
-        //      5 points
-        //          TODO #4     was the sqFt field entered?
-        //                      If so call a method to find homes that are less than or equal to that size
-        //                      EXTRA 5 points if you add to the criteria string so it contains the information about
-        //                                     what we are searching for. Do this for each search field
+        if (sqFt != null && sqFt > 0) {
+            matchedHomes = matchedHomes.stream().filter(l -> l.getSqFt() >= sqFt).collect(Collectors.toList());
+            criteria += ", with at least: " + sqFt + " Sq Ft";
+        }
 
-        //      5 points
-        //          TODO #5     was the price field entered?
-        //                          If so call a method to find homes that are less than or equal to that price
+        if (price != null && price > 0) {
+            matchedHomes = matchedHomes.stream().filter(l -> l.getPrice() <= price).collect(Collectors.toList());
+            criteria += ", for less than $" + price;
+        }
 
-        //      5 points
-        //          TODO #6     was the beds field entered?
-        //                          If so call a method to find homes that have at least that many bed rooms
+        if (beds != null && beds > 0) {
+            matchedHomes = findByBeds(matchedHomes, beds);
+            criteria += ", " + beds + "+ beds";
+        }
 
         model.addAttribute("title", "Home Listings");
         model.addAttribute("criteria", criteria);
         model.addAttribute("listings", matchedHomes);
-        return "listListings";
+
+        model.addAttribute("city", city);
+        model.addAttribute("beds", beds);
+
+        return "listings";
     }
 
-    //  10 points
-    //      TODO #7      Create a method that will find listings that match the city name entered in the search field
-    public List<Listing> findByCity(List<Listing> list, String city) {
-        //  you will add in your code to search for matching cities
-        return null;            //  TODO !! DELETE THIS LINE OF CODE. It is only here so the app will compile
+    /**
+     *
+     * @param list      a list of homes
+     * @param beds      minimum number of beds
+     * @return          a list of homes with at least the number of beds desired
+     *
+     *      This is a method to loop through all the homes in the provided list
+     *      If the home has at least the number of beds requested
+     *      save that home to the list of suggested homes for the home buyer
+     */
+    public List<Listing> findByBeds(List<Listing> list, int beds) {
+        //  shortList will contain the listings with at least the number of bed rooms searched for
+        List<Listing> shortList = new ArrayList<>();
+
+        //  look at all listings. One at a time
+        for (Listing Listing : list) {
+            //  check to see if the first or last name contains the name given
+            //  we will convert the first and last names to lowercase since contains does a case sensitive compare
+            if (Listing.getBeds() >= beds) {
+                shortList.add(Listing);
+            }
+        }
+        //  return the list of Listings we found matching the name provided
+        return shortList;
     }
-
-    //  10 points
-    //      TODO #8      Create a method that will find listings that are less than or equal to the square footage entered in the search field
-
-    //  10 points
-    //      TODO #9      Create a method that will find listings that are priced below what was entered in the search field
-
-    //  10 points
-    //      TODO #10      Create a method that will find listings that have at least the number of bed rooms entered in the search field
 }
